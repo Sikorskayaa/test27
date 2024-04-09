f51607928cfeaae9adf5bb688526edc68e8060b1
Sikorskayaa <133200518+Sikorskayaa@users.noreply.github.com>
2024-04-09 17:35:04 +0200

    movies

--- js/test.js
+++ js/test.js
@@ -1,182 +1,75 @@
-// function proccesCall(recipient, onAvailable, onNotAvailable) {
-//   const isRecipientAvailable = Math.random() > 0.6;
+// https://image.tmdb.org/t/p/w500/1pdfLvkbY9ohJlCjQH2CZjjYVvJ.jpg - img
 
-//   if (!isRecipientAvailable) {
-//     onNotAvailable(recipient);
-//     return;
-//   }
+const refs = {
+  list: document.querySelector(".js-movie-list"),
+  loadMore: document.querySelector(".js-load-more"),
+};
 
-//   onAvailable(recipient);
-// }
+let page = 1;
 
-// function takeCall(name) {
-//   console.log(`З'єднуємо з ${name},очікуйте...`);
-// }
+refs.loadMore.addEventListener("click", onLoadMore);
 
-// function activateAnsweringMachine(name) {
-//   console.log(`Абонент ${name} недоступний,залиште повідомлення.`);
-// }
+serviceMovie()
+  .then((data) => {
+    console.log(data);
+    console.log(data.page);
+    console.log(data.total_pages);
 
-// function leaveHoloMessage(name) {
-//   console.log(`Абонент ${name} недоступний,записуємо голограму.`);
-// }
+    refs.list.insertAdjacentHTML("beforeend", createMarkup(data.results));
 
-// proccesCall("Mango", takeCall, activateAnsweringMachine);
-// proccesCall("Poli", takeCall, leaveHoloMessage);
+    if (data.page < data.total_pages) {
+      refs.loadMore.classList.replace("load-more-hidden", "load-more");
+    }
+  })
+  .catch((error) => console.log(error));
 
-// const books = [
-//   {
-//     title: "The Last Kingdom",
-//     author: "Bernard Cornwell",
-//     genres: ["historical prose", "adventure"],
-//     rating: 8.38,
-//   },
-//   {
-//     title: "На березі спокійних вод",
-//     author: "Роберт Шеклі",
-//     rating: 8.51,
-//   },
-// ];
+function onLoadMore({ target }) {
+  page += 1;
+  target.disabled = true;
 
-// let totalRating = 0;
+  serviceMovie(page)
+    .then((data) => {
+      refs.list.insertAdjacentHTML("beforeend", createMarkup(data.results));
 
-// for (const book of books) {
-//   totalRating += book.rating;
-// }
+      if (data.page >= data.total_pages) {
+        refs.loadMore.classList.replace("load-more", "load-more-hidden");
+      }
+    })
+    .catch((error) => console.log(error))
+    .finally(() => (target.disabled = false));
+}
 
-// const avarageRating = (totalRating / books.length).toFixed(1);
+function serviceMovie(page = 1) {
+  const BASE_URL = "https://api.themoviedb.org/3";
+  const API_KEY = "92a9a9e3708a3e9451b7037d5906879a";
+  const END_POINT = "trending/movie/week";
 
-// console.log(avarageRating);
+  const params = new URLSearchParams({
+    api_key: API_KEY,
+    page,
+  });
 
-// const numbers = [5, 10, 15, 20, 25];
+  return fetch(`${BASE_URL}/${END_POINT}?${params}`).then((resp) => {
+    if (!resp.ok) {
+      throw new Error(`Fetch error with ${resp.status}: ${resp.statusText}`);
+    }
+    return resp.json();
+  });
+}
 
-// Класичний for
-// for (let i = 0; i < numbers.length; i += 1) {
-//   console.log(`Індекс ${i}, значення ${numbers[i]}`);
-// }
-/*==========================================*/
-
-// numbers.forEach(function (number, i) {
-//   console.log(`Індекс ${number}, значення ${numbers[i]}`);
-// });
-
-// const students = [
-//   { name: "Манго", score: 83 },
-//   { name: "Ківі", score: 59 },
-//   { name: "Аякс", score: 37 },
-//   { name: "Ківі", score: 94 },
-//   { name: "Х'юстон", score: 64 },
-//   { name: "Ківі", score: 62 },
-// ];
-
-// МЕТОДЫ Map() AND FILTER()
-
-// const namesStudent = students
-//   .map((student) => student.name)
-//   .filter((i, index, array) => array.indexOf(i) === index);
-// console.log(namesStudent);
-
-// const studentScore = students
-//   .map((student) => student.score)
-//   .filter((score) => score >= 60)
-//   .sort((a, b) => b - a);
-// console.log(studentScore);
-
-// const okName = students.findIndex((student) => student.name === "Манго");
-// console.log(okName);
-
-/*======================THIS===================*/
-
-// const polina = {
-//   username: "Polina",
-
-//   showName() {
-//     console.log(this.username);
-//   },
-// };
-// polina.showName();
-
-// ===============CALL===================
-
-// function greetGuest(greeting) {
-//   console.log(`${greeting}, ${this.username} у наш готель ${this.hotel}.`);
-// }
-
-// const mango = {
-//   username: "Манго",
-//   hotel: "Plaza",
-// };
-// const poly = {
-//   username: "Полі",
-//   hotel: "Delfin",
-// };
-
-// greetGuest.call(mango, "Ласкаво просимо");
-// greetGuest.call(poly, "З прибуттям");
-
-// ==================BIND================
-// function greet(clientName) {
-//   return `${clientName}, ласкаво просимо в «${this.service}».`;
-// }
-
-// const steam = {
-//   service: "Steam",
-// };
-// const steamGreeter = greet.bind(steam);
-// console.log(steamGreeter("Манго"));
-
-// const gmail = {
-//   service: "Gmail",
-// };
-// const gmailGreeter = greet.bind(gmail);
-// console.log(gmailGreeter("Полі"));
-
-// =====CALLBACK BIND=============
-
-// const customer = {
-//   firstName: "Jacob",
-//   lastName: "Mercer",
-//   getFullName() {
-//     return `${this.firstName} ${this.lastName}`;
-//   },
-// };
-
-// function makeMessage(callback) {
-//   callback();
-//   console.log(`Обробляємо заявку від ${callback()}.`);
-// }
-
-// makeMessage(customer.getFullName.bind(customer));
-
-// const saveBtn = document.querySelector(".btn");
-
-// const handleBtn = (event) => {
-//   console.log(event);
-// };
-// saveBtn.addEventListener("click", handleBtn);
-// const registerForm = document.querySelector(".form");
-
-// registerForm.addEventListener("submit", handleSubmit);
-
-// function handleSubmit(event) {
-//   event.preventDefault();
-//   const form = event.target;
-//   const login = form.elements.login.value;
-//   const password = form.elements.password.value;
-
-//   if (login === "" || password === "") {
-//     return console.log("Please fill in all the fields!");
-//   }
-
-//   console.log(`Login: ${login}, Password: ${password}`);
-//   form.reset();
-// }
-
-// const textInput = document.querySelector(".text-input");
-// const output = document.querySelector(".output");
-
-// const handleInput = (event) => {
-//   output.textContent = event.currentTarget.value;
-// };
-
-// textInput.addEventListener("input", handleInput);
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ poster_path, original_title, release_date, vote_average }) => `
+      <li class='movie-card'>
+      <img src="https://image.tmdb.org/t/p/w500${poster_path}" alt="${original_title}" width=200>
+  <div class="movie-info">
+    <h2>${original_title}</h2>
+    <p>Release Date: ${release_date}</p>
+    <p>Vote Average: ${vote_average}</p>
+  </div>
+</li>
+    `
+    )
+    .join("");
+}
